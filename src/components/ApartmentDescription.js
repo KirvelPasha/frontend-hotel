f59181a431939f71193201f198729e052efd1eda
Pasha <kirvelpasha99@gmail.com>
2020-04-26 11:41:32 +0300

    Fix Impl get apartment by id

--- src/components/ApartmentDescription.js
+++ src/components/ApartmentDescription.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import {withRouter} from "react-router"
-class ApartmentDescription extends React.Component {
+import axios from "axios";
 
+class ApartmentDescription extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            apartment: props.apartment
+            apartment: Object
         }
+    }
+
+    componentDidMount() {
+        this.getApartment();
+    }
+
+    getApartment() {
+        let id = this.props.match.params.id;
+        axios.get("http://localhost:8081/apartments/" + id)
+            .then(response => response.data)
+            .then((data) => {
+                this.setState({apartment: data})
+            })
     }
 
     render() {
         return (
             <div className="bg-dark text-white">
-                <h1>{this.props.match.params.id}</h1>
+                <h1>{this.state.apartment.id}</h1>
             </div>
 
         );
--- src/components/ApartmentList.js
+++ src/components/ApartmentList.js
@@ -8,61 +8,60 @@ class ApartmentList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            apartments : []
+            apartments: []
         }
     }
 
     componentDidMount() {
-       this.getApartments();
+        this.getApartments();
 
     }
 
     getApartments() {
         axios.get("http://localhost:8081/apartments")
             .then(response => response.data)
-            .then((data) =>{
+            .then((data) => {
                 this.setState({apartments: data})
             })
     }
 
     render() {
         return (
-           <Card className="border border-dark bg-dark text-white">
-               <Card.Header>Apartment List</Card.Header>
-               <Card.Body>
-                   <Table bordered hover striped variant="dark">
-                       <thead>
-                       <tr>
-                           <th>#</th>
-                           <th>Count Rooms</th>
-                           <th>Count Places</th>
-                           <th>Price</th>
-                           <th>More</th>
-                       </tr>
-                       </thead>
-                       <tbody>
-                       {
-                           this.state.apartments.length === 0 ?
-                               <tr align="center">
-                                   <td colSpan="6">No Apartments</td>
-                                   <td>
-                                       <Link to="/apartments/1" className="nav-link">Read</Link>
-                                   </td>
-                               </tr>:
-                           this.state.apartments.map((apartment) => (
-                               <tr key={apartment.id}>
-                                   <td>{apartment.id}</td>
-                                   <td>{apartment.countRooms}</td>
-                                   <td>{apartment.countPlaces}</td>
-                                   <td>{apartment.price}</td>
-
-                               </tr>
-                               ))
-                       }
-                       </tbody>
-                   </Table>
-               </Card.Body>
-           </Card>
+            <Card className="border border-dark bg-dark text-white">
+                <Card.Header>Apartment List</Card.Header>
+                <Card.Body>
+                    <Table bordered hover striped variant="dark">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Count Rooms</th>
+                            <th>Count Places</th>
+                            <th>Price</th>
+                            <th>More</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.apartments.length === 0 ?
+                                <tr align="center">
+                                    <td colSpan="6">No Apartments</td>
+                                </tr> :
+                                this.state.apartments.map((apartment) => (
+                                    <tr key={apartment.id}>
+                                        <td>{apartment.id}</td>
+                                        <td>{apartment.countRooms}</td>
+                                        <td>{apartment.countPlaces}</td>
+                                        <td>{apartment.price}</td>
+                                        <td>
+                                            <Link to={"/apartments/" + apartment.id} className="nav-link">Read</Link>
+                                        </td>
+                                    </tr>
+                                ))
+                        }
+                        </tbody>
+                    </Table>
+                </Card.Body>
+            </Card>
         );
     }
 }
