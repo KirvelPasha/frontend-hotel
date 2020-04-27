9894acdd0ca62ae38f5848ac02671800feb3dd1d
Pasha <kirvelpasha99@gmail.com>
2020-04-27 18:49:10 +0300

    add

--- src/components/Join.js
+++ src/components/Join.js
@@ -9,6 +9,7 @@ class Join extends React.Component {
         this.state = {
             name: '',
             surname: '',
+            login: '',
             email: '',
             password: ''
         }
@@ -18,6 +19,7 @@ class Join extends React.Component {
         this.setState({
             name: e.target.value,
             surname: e.target.value,
+            login: e.target.value,
             email: e.target.value,
             password: e.target.value
         })
@@ -36,22 +38,27 @@ class Join extends React.Component {
         return (
             <Form onSubmit={this.handleSubmit}>
                 <Form.Group controlId="formGroupName">
-                    <Form.Label>Email address</Form.Label>
+                    <Form.Label className="text-white">Name</Form.Label>
                     <Form.Control type="name" placeholder="Enter name" name="name" autoComplete="off"
                                   onChange={this.handleChange}/>
                 </Form.Group>
                 <Form.Group controlId="formGroupSurname">
-                    <Form.Label>Password</Form.Label>
+                    <Form.Label className="text-white">Surname</Form.Label>
                     <Form.Control type="surname" placeholder="Enter surname" name="surname" autoComplete="off"
                                   onChange={this.handleChange}/>
                     <Form.Group controlId="formGroupEmail">
-                        <Form.Label>Email address</Form.Label>
+                        <Form.Label className="text-white">Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" name="email" autoComplete="off"
+                                      onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="formGroupLogin">
+                        <Form.Label className="text-white">Login</Form.Label>
+                        <Form.Control type="login" placeholder="login" name="login" autoComplete="off"
                                       onChange={this.handleChange}/>
                     </Form.Group>
                 </Form.Group>
                 <Form.Group controlId="formGroupPassword">
-                    <Form.Label>Password</Form.Label>
+                    <Form.Label className="text-white">Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" name="password" autoComplete="off"
                                   onChange={this.handleChange}/>
                 </Form.Group>
